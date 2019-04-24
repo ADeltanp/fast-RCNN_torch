0ea28d7fbd86190c91d6fc60f7883398cb4fc5a5
ADeltanp <deltaph@hotmail.com>
2019-04-24 11:13:06 +0800

    working on it

---
+++ Models/VGG16.py
@@ -0,0 +1,8 @@
+import torch as t
+import torch.nn as nn
+import torch.functional as F
+
+class VGG16(nn.Module):
+    def __init__(VGG16, self):
+        super(VGG16, self).__init__()
+        
---
+++ utils/ProposalLayer.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch as t
+import torch.nn as nn
+
+class ProposalLayer:
+    def __init__(self,
+                 extractor,
+                 nms_thresh=0.7,
+                 n_pre_nms_train = 12000,
+                 n_post_nms_train = 2000,
+                 n_pre_nms_test = 6000,
+                 n_post_nms_test = 300,
+                 min_bbox_size = 16):
+        self.extractor = extractor
+        self.nms_thresh  = nms_thresh
+        self.n_pre_nms_train = n_pre_nms_train
+        self.n_post_nms_train = n_post_nms_train
+        self.n_pre_nms_test = n_pre_nms_test
+        self.n_post_nms_test = n_post_nms_test
+        self.min_bbox_size = min_bbox_size
+
+    def __call__(self, cls, reg, anchor, img_size, img_scale):
+        
+
